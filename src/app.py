13a942bad436cf195d58bac6838c42f51116dd23
Kim Bahir Andersen <kim@bahir.dk>
2020-04-12 14:56:07 +0200

    Testing setting environment variables from yaml

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -6,6 +7,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello world, Bahir"
+
+
+@app.route("/env")
+def env():
+    try:
+        return os.environ['DEMO_ENV_VAR']
+    except Exception:
+        return "Key not found :("
 
 
 if __name__ == "__main__":
